883a55a9c58eb3afe2dcd63e91a6589c8f757e6f
Robbie Mitchell <robbie@paywithcover.com>
2016-03-16 10:10:40 -0400

    Append output to a file
    Add repo to each row, and add comma delimter

--- gitmail.rb
+++ gitmail.rb
@@ -5,7 +5,7 @@ class Finder
     Dir.mktmpdir("GitMail") do |dir|
       `git clone --bare #{ARGV[1] || "https://github.com/"}#{repo_name}.git #{dir}`
       Dir.chdir("#{dir}") do
-        @emails = `git log --pretty=format:'%an %ae'`
+        @emails = `git log --pretty=format:'%an, %ae, #{repo_name}'`
       end
     end
   end
@@ -17,5 +17,8 @@ end
 
 if $PROGRAM_NAME == __FILE__
   repo_name = ARGV[0]
-  puts Finder.new(repo_name).all_contributors
+  open(ENV['HOME']+'/projects/contributors.csv', 'a') { |f|
+    f.puts Finder.new(repo_name).all_contributors
+  }
+  ## puts Finder.new(repo_name).all_contributors
 end
